1f853712d05dc5baeb167b2123067ab3bb2dd1c3
h7x4 <h7x4abk3g@protonmail.com>
2021-04-18 15:06:50 +0200

    Add first functions

---
+++ src/App/Program.fs
@@ -0,0 +1,25 @@
+module Bot.Program
+
+open Microsoft.Azure.WebJobs
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Logging
+open Microsoft.AspNetCore.Mvc
+
+[<FunctionName("Ping")>]
+let ping
+    ([<HttpTrigger()>] request: HttpRequest)
+    (logger: ILogger) =
+    async {  
+        logger.LogWarning("Pong")
+        return new OkObjectResult("Pong")
+    }
+    |> Async.StartAsTask
+
+[<FunctionName("Pong")>]
+let pong
+    ([<TimerTrigger("*/10 * * * * *", RunOnStartup = true)>] timerInfo: TimerInfo)
+    (logger: ILogger) =
+    async {        
+        logger.LogWarning("Pong")
+    }
+    |> Async.StartAsTask
