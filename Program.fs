42fd7d1934e76592727a8789a91c2f75371a98e2
Martin Skatvedt <martinskatvedt@gmail.com>
2021-04-18 17:25:04 +0200

    added logging

--- Program.fs
+++ Program.fs
@@ -4,7 +4,8 @@ open Microsoft.Azure.WebJobs
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Logging
 open Microsoft.AspNetCore.Mvc
-
+open FSharp.Json
+open System.IO
 [<FunctionName("Ping")>]
 let ping
     ([<HttpTrigger()>] request: HttpRequest)
@@ -15,25 +16,50 @@ let ping
     }
     |> Async.StartAsTask
 
-
-
-
 // [<FunctionName("Pong")>]
-let pong
-    ([<TimerTrigger("*/10 * * * * *", RunOnStartup = true)>] timerInfo: TimerInfo)
-    (logger: ILogger) =
-    async {        
-        logger.LogWarning("Pong")
-    }
-    |> Async.StartAsTask
+//let pong
+//    ([<TimerTrigger("*/10 * * * * *", RunOnStartup = true)>] timerInfo: TimerInfo)
+//    (logger: ILogger) =
+//    async {        
+//       logger.LogWarning("Pong")
+//    }
+//    |> Async.StartAsTask
 
+
+type teamType = {
+    id: string
+    domain: string
+}
+
+type userType =  {
+    id: string
+    username: string
+    team_id: string
+}
+
+type requestType = {
+  token: string
+  action_ts: string
+  team: teamType
+  user: userType
+  callback_id: string
+  trigger_id: string
+}
 
 [<FunctionName("ShortcutResponse")>]
 let ShortcutResponse
     ([<HttpTrigger()>] request: HttpRequest)
     (logger: ILogger) =
-    async {  
-        return new OkObjectResult()
+    async {
+        let sr = new StreamReader(request.Body)
+        let s = sr.ReadToEndAsync() |> Async.AwaitTask |> Async.RunSynchronously
+        logger.LogWarning(
+           s
+        )  
+        //let req = Json.deserialize<requestType> 
+
+
+        return new OkObjectResult("ok")
     }
     |> Async.StartAsTask
 
