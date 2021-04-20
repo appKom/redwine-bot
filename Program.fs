fd7e1402da81fe9dd3439873536fab772e8d7e62
Martin Skatvedt <martinskatvedt@gmail.com>
2021-04-20 20:55:31 +0200

    now sends back modal

--- Program.fs
+++ Program.fs
@@ -6,8 +6,10 @@ open Microsoft.Extensions.Logging
 open Microsoft.AspNetCore.Mvc
 open System.IO
 open FSharp.Data
-open FSharp.Data.HttpRequestHeaders
 
+open Hopac
+open HttpFs.Client
+open FSharp.Data
 type provider = JsonProvider<""" {"type":"shortcut","token":"xxx","action_ts":"xxx","team":{"id":"xxx","domain":"xxx"},"user":{"id":"xxx","username":"xxx","team_id":"xxx"},"is_enterprise_install":false,"enterprise":null,"callback_id":"getWine","trigger_id":"xxx"} """>
 
 [<FunctionName("ShortcutResponse")>]
@@ -21,42 +23,70 @@ let ShortcutResponse
         let triggerId = jsonData.TriggerId
 
         logger.LogWarning(jsonData.Type)
+        let multipartRequest =
+            Request.createUrl Post "https://slack.com/api/views.open"
+            |> Request.setHeader (ContentType (ContentType.create("application", "json", HttpEncodings.PostDefaultEncoding)))
+            |> Request.setHeader (Authorization "Bearer ")
+            |> Request.bodyString( "{ trigger_id: " + $""" "{jsonData.TriggerId}", """ + """  
+                "view" : {
+                  "type": "modal",
+                  "title": {
+                    "type": "plain_text",
+                    "text": "Modal title"
+                  },
+                  "blocks": [
+                    {
+                      "type": "section",
+                      "text": {
+                        "type": "mrkdwn",
+                        "text": "It's Block Kit...but _in a modal_"
+                      },
+                      "block_id": "section1",
+                      "accessory": {
+                        "type": "button",
+                        "text": {
+                          "type": "plain_text",
+                          "text": "Click me"
+                        },
+                        "action_id": "button_abc",
+                        "value": "Button value",
+                        "style": "danger"
+                      }
+                    },
+                    {
+                      "type": "input",
+                      "label": {
+                        "type": "plain_text",
+                        "text": "Input label"
+                      },
+                      "element": {
+                        "type": "plain_text_input",
+                        "action_id": "input1",
+                        "placeholder": {
+                          "type": "plain_text",
+                          "text": "Type in here"
+                        },
+                        "multiline": false
+                      },
+                      "optional": false
+                    }
+                  ],
+                  "close": {
+                    "type": "plain_text",
+                    "text": "Cancel"
+                  },
+                  "submit": {
+                    "type": "plain_text",
+                    "text": "Save"
+                  },
+                  "private_metadata": "Shhhhhhhh",
+                  "callback_id": "view_identifier_12"
+                },
+            }""")
+            |> Request.responseAsString
+            |> run
 
-   
-        // let requestString = (Http.RequestString("https://slack.com/api/views.open", httpMethod = "POST",
-        //         headers = [ ContentType HttpContentTypes.Json; Authorization "" ],
-        //         body = TextRequest """ {
-        //           "trigger_id": {triggerId},
-        //           "view": {
-        //             "type": "modal",
-        //             "callback_id": "modal-identifier",
-        //             "title": {
-        //               "type": "plain_text",
-        //               "text": "Just a modal"
-        //             },
-        //             "blocks": [
-        //               {
-        //                 "type": "section",
-        //                 "block_id": "section-identifier",
-        //                 "text": {
-        //                   "type": "mrkdwn",
-        //                   "text": "*Welcome* to ~my~ Block Kit _modal_!"
-        //                 },
-        //                 "accessory": {
-        //                   "type": "button",
-        //                   "text": {
-        //                     "type": "plain_text",
-        //                     "text": "Just a button"
-        //                   },
-        //                   "action_id": "button-identifier"
-        //                 }
-        //               }
-        //             ]
-        //           }
-        //         } """ 
-        //     ))
-                 
-        //logger.LogWarning(response.ResponseUrl)
+        printfn "Response of get is %s: " multipartRequest
 
         return new OkObjectResult("ok")
     }
